e0cb41b3ba2862777e7246242034beba82ad21ee
aditya-dixit-binary <aditya.dixit@binary.com>
2019-12-29 20:55:59 +0530

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+cp .zshrc ~/.zshrc
+source .zshrc
