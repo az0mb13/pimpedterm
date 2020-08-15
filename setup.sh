b10ed7ed7c243c4f83bdd435c51fa2eaefce0cab
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2020-08-15 12:19:57 +0530

    Updated some commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
+sudo apt-get install zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
+zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 cp .zshrc ~/.zshrc
@@ -13,4 +12,4 @@ source .p10k.zsh
 
 chsh -s /usr/bin/zsh $(whoami)
 
-echo "Reboot it plx"
+echo "Reboot it plx or run p10k configure again"
